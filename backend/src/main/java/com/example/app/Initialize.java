23ea117d53271cdc52cd89be19a26f733b6b091d
Dmitriy <dima.poshevelya@mail.ru>
2020-02-28 19:44:10 +0300

    First commit. Add some files. Add SpringBoot and Qulice for this project.

---
+++ backend/src/main/java/com/example/app/Initialize.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright
+ */
+
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+
+
+public class Initialize {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConfig.class, args);
+    }
+}
---
+++ backend/src/main/java/com/example/app/SpringBootConfig.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright
+ */
+
+package com.example.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.example.app")
+public class SpringBootConfig {
+}
