523c253d1b3059c848bd5987baf60bdaa5f9dc59
Dmitriy <dima.poshevelya@mail.ru>
2020-03-01 13:20:04 +0300

    Fix project structure

--- backend/wrest/src/main/java/com/netcracker/shop/init/EntryPoint.java
+++ backend/wrest/src/main/java/com/netcracker/shop/init/EntryPoint.java
@@ -1,13 +1,8 @@
-/*
- * Copyright
- */
-
-package com.example.app;
+package com.netcracker.shop.init;
 
 import org.springframework.boot.SpringApplication;
 
-
-public class Initialize {
+public class EntryPoint {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootConfig.class, args);
     }
---
+++ backend/wrest/src/main/java/com/netcracker/shop/init/SpringBootConfig.java
@@ -0,0 +1,7 @@
+package com.netcracker.shop.init;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.netcracker.shop")
+public class SpringBootConfig {
+}
